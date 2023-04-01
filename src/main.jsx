90c5b6aa5553c337ce35adb0f666122dda63fc98
bora-sen <bahattinborasen@gmail.com>
2023-04-01 19:03:57 +0300

    +added media

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,13 @@
 import React from "react"
 import ReactDOM from "react-dom/client"
+import { BrowserRouter } from "react-router-dom"
 import "./index.css"
+import MainRouter from "./Routes/MainRouter"
 
 ReactDOM.createRoot(document.getElementById("root")).render(
     <React.StrictMode>
-        <App />
+        <BrowserRouter>
+            <MainRouter />
+        </BrowserRouter>
     </React.StrictMode>
 )
