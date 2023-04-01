b21e2d7c513fbf2abd63300e7d9fab4f9567f9bd
bora-sen <bahattinborasen@gmail.com>
2023-04-01 19:03:03 +0300

    +update configure packages

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,14 @@
 export default {
     content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
     theme: {
-        extend: {},
+        extend: {
+            backgroundImage: {
+                main: "url(/assets/media/landing-background.jpg)",
+            },
+            fontFamily: {
+                "open-sans": ["Open Sans", "sans-serif"],
+            },
+        },
     },
     plugins: [],
 }
