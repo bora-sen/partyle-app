c585fcf92d689951561b68e3d09d07d745137d63
bora-sen <bahattinborasen@gmail.com>
2023-05-14 18:24:37 +0300

    +updated app

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,14 @@ import ReactDOM from "react-dom/client"
 import { BrowserRouter } from "react-router-dom"
 import "./index.css"
 import MainRouter from "./Routes/MainRouter"
+import AuthProvider from "./Context/AuthContext"
 
 ReactDOM.createRoot(document.getElementById("root")).render(
     <React.StrictMode>
-        <BrowserRouter>
-            <MainRouter />
-        </BrowserRouter>
+        <AuthProvider>
+            <BrowserRouter>
+                <MainRouter />
+            </BrowserRouter>
+        </AuthProvider>
     </React.StrictMode>
 )
