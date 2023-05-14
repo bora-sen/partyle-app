6a2d16ac0be22b3a07f8adc8209ade38186406ef
bora-sen <bahattinborasen@gmail.com>
2023-05-14 18:23:55 +0300

    +added Routing

--- src/Routes/MainRouter.jsx
+++ src/Routes/MainRouter.jsx
@@ -1,11 +1,64 @@
 import React from "react"
 import { Route, Routes } from "react-router-dom"
-import { Landing } from "../Pages"
+import {
+    CreateEvent,
+    EventPage,
+    Events,
+    Landing,
+    Login,
+    MyTickets,
+    SignUp,
+    Ticket,
+} from "../Pages"
+import ProtectedRoute from "./ProtectedRoute"
 
 function MainRouter() {
     return (
         <Routes>
-            <Route path="/" element={<Landing />} />
+            <Route path="/welcome" element={<Landing />} />
+            <Route
+                path="/"
+                element={
+                    <ProtectedRoute redUrl="/welcome">
+                        <Events />
+                    </ProtectedRoute>
+                }
+            />
+            <Route
+                path="/events"
+                element={
+                    <ProtectedRoute redUrl="/login">
+                        <Events />
+                    </ProtectedRoute>
+                }
+            />
+            <Route
+                path="/mytickets"
+                element={
+                    <ProtectedRoute redUrl="/login">
+                        <MyTickets />
+                    </ProtectedRoute>
+                }
+            />
+            <Route
+                path="/ticket/:publicId"
+                element={
+                    <ProtectedRoute redUrl="/login">
+                        <Ticket />
+                    </ProtectedRoute>
+                }
+            />
+            <Route path="/signup" element={<SignUp />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/create" element={<CreateEvent />} />
+            <Route
+                path="/event/:uuid"
+                element={
+                    <ProtectedRoute redUrl="/login">
+                        <EventPage />
+                    </ProtectedRoute>
+                }
+            />
         </Routes>
     )
 }
--- src/Routes/ProtectedRoute.jsx
+++ src/Routes/ProtectedRoute.jsx
@@ -1,5 +1,15 @@
-function ProtectedRoute({ children }) {
-    return { children }
+import { useContext } from "react"
+import { AuthContext } from "../Context/AuthContext"
+import { Navigate } from "react-router-dom"
+
+function ProtectedRoute({ redUrl, children }) {
+    const { user } = useContext(AuthContext)
+
+    if (user !== false) {
+        return children
+    } else {
+        return <Navigate to={redUrl} />
+    }
 }
 
 export default ProtectedRoute
